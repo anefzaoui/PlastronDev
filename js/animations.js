6ead2ead01489e01d8df3439f15b5140a16166d9
Ahmed Nefzaoui <nefzaoui@debian.lan>
2014-05-03 22:18:53 +0100

    First commit.

---
+++ js/animations.js
@@ -0,0 +1,26 @@
+var Animation = {
+  
+  RTL : false,
+  
+  /*
+   * Give it current as the current
+   * layer's ID and next as the next
+   * layer's ID
+   * */
+	nextLayer : function(current,next){
+    document.getElementById(current).classList.remove("current");
+    document.getElementById(current).classList.add("previous");
+    
+    //document.getElementById(next).classList.remove("previous");
+    document.getElementById(next).classList.add("current");
+    
+  },
+  
+  previousLayer : function(current,previous){
+    document.getElementById(current).classList.add("goForwardOneStep");
+    document.getElementById(previous).classList.add("BecomeCurrentFromBackward");
+  }
+	
+	
+	
+};
---
+++ js/utils.js
@@ -0,0 +1,66 @@
+function $(id){
+  return document.getElementById(id);
+}
+
+var utils = {
+
+  // Add all event listeners for all buttons in App.
+  btnListeners : function(){
+    $("NextToPass").addEventListener('click', function(evt) {
+      Animation.nextLayer('setup','password');
+    });
+    $("NextToConfigDone").addEventListener('click', function(evt) {
+      Animation.nextLayer('password','config_done');
+    });
+    
+    // Check if password is true
+    $("CheckPassForContent").addEventListener('click', function(evt) {
+      asyncStorage.getItem('__CONFIG_PASSWORD__', function(value) {
+        var inputValue = $("valueCheckPassForContent").value;
+        if(value == inputValue){
+          $("errorWrongPasscode").classList.add("hidden");
+          console.log("Passed");
+        }
+        else{
+          $("errorWrongPasscode").classList.remove("hidden");
+        }
+      });
+    });
+    
+    
+  },
+
+  // Checks if there are previous configurations to decide whether start First Run Dialog or not.
+  configured: function(){
+    asyncStorage.getItem('__CONFIG_', function(value) {
+        if(value == "true")
+          $("password_enter").classList.add("root");
+        else
+          $("setup").classList.add("root");
+    });
+  },
+
+  addRootPanel: function(){
+  },
+
+  setPassword: function(password){
+	  asyncStorage.setItem('__CONFIG_PASSWORD__', password);
+	  asyncStorage.setItem('__CONFIG_', 'true');
+  },
+
+  getPassword: function(){
+	asyncStorage.getItem('__CONFIG_PASSWORD__', function(value) {
+		
+    });
+  }
+
+
+};
+
+
+window.addEventListener('load', function readerOnLoad(evt) {
+  window.removeEventListener('load', readerOnLoad);
+  utils.configured();
+  utils.btnListeners();
+  
+});
